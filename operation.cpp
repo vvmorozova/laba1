a7b488ac02d6518a9bb16fa514fca209e145a2c7
vvmorozova <74507613+vvmorozova@users.noreply.github.com>
2020-11-16 16:11:52 +0300

    Update operation.cpp

--- operation.cpp
+++ operation.cpp
@@ -1,22 +1,39 @@
-#include "complexnumber.h"
-#ifndef OPERATION_H
-#define OPERATION_H
+#include "operation.h"
 
-
-class Operation : public Element
+Operation::Operation(int quantity, char sign)
 {
-private:
-    int operandQuantity;
-    char operationSign;
-public:
-    Operation(int quantity, char sign);
-    bool verifyInput(int quantity, char sign) const;
-    int getOperandQuantity() const;
-    char getOperationSign() const;
-    void setOperandQuantity(int number);
-    void setOperationSign(char symbol);
-    ~Operation();
-};
+    this->operandQuantity=quantity;
+    this->operationSign=sign;
+}
 
+bool Operation::verifyInput(int quantity, char sign) const
+{
+    if (quantity>1 && sign=='=') return false;
+    else return true;
+}
 
-#endif // OPERATION_H
+int Operation::getOperandQuantity() const
+{
+    return this->operandQuantity;
+}
+
+char Operation::getOperationSign() const
+{
+    return this->operationSign;
+}
+
+void Operation::setOperandQuantity(int number)
+{
+    this->operandQuantity=number;
+}
+
+void Operation::setOperationSign(char symbol)
+{
+    this->operationSign=symbol;
+}
+
+Operation::~Operation()
+{
+    operandQuantity=0;
+    operationSign='0';
+}
